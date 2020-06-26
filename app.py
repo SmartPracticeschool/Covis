6ccd1eab5c4880e77454817af20a30c6615fbdd4
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-06-26 07:15:40 +0530

    Creation of Flask app; app.py updated

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import numpy as np
 from plot import Plot
 from data_plot_labels import data_plot
 import plotly.graph_objects as go
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
 
 # Loading the dataset
 df = pd.read_csv('Model_training/Date_Sentiments.csv')
@@ -20,8 +23,17 @@ def lockdown_bar(figure):
                         name=['Positive', 'Negative', 'Neutral'],
                         color=['rgb(128,0,128)', 'rgb(26,118,255)', 'rgb(178,34,34)'])
 
-    figure.plotting(title='Sentiments in India',
+    final_plot = figure.plotting(title='Sentiments in India',
                     x_title='Dates',
                     y_title='Sentiments',
                     )
-    return figure
+    return final_plot
+
+plot1 = lockdown_bar(fig)
+
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('home.html', plot1=plot1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
