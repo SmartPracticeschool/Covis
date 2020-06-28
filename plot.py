a5bfc38485c65b270051d0f4aaff7f4d1ca2b9c3
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-06-29 03:54:46 +0530

    Added functionality of dropdown in graphs

--- plot.py
+++ plot.py
@@ -48,7 +48,170 @@ class Plot():
             fig.add_trace(traces)
 
         fig.update_layout(
-            title=title,
+            title={
+                'text': title,
+                'y': 0.99,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': dict(
+                    family='Rockwell',
+                    color='dim gray',
+                    size=15),
+            }, template='ggplot2',
+            xaxis=dict(
+                title=x_title,
+                tickfont_size=14,
+                tickangle=90,
+                tickfont=dict(
+                    family='Rockwell',
+                    color='dim gray',
+                    size=9),
+                tickmode='linear',
+                range=x_range,
+            ),
+            yaxis=dict(
+                title=y_title,
+                titlefont_size=16,
+                tickfont_size=14,
+            ),
+            legend=dict(
+                x=0.8,
+                y=1.0,
+                bgcolor='rgba(255, 255, 255, 0)',
+                bordercolor='rgba(255, 255, 255, 0)'
+            ),
+            barmode='group',
+            bargap=0.2,
+            bargroupgap=0.1,
+            plot_bgcolor='rgb(248,248,255)',
+            
+            # adding dropdown
+            updatemenus=[
+                dict(
+                    buttons=list([
+                        dict(
+                            label="Cummulative",
+                            method="update",
+                            args=[{"visible": [True, True, True]},
+                                ]),
+                        dict(
+                            label="Positive",
+                            method="update",
+                            args=[{"visible": [True, False, False]},
+                                ]),
+                        dict(
+                            label="Negative",
+                            method="update",
+                            args=[{"visible": [False, True, False]},
+                                ]),
+                        dict(
+                            label="Neutral",
+                            method="update",
+                            args=[{"visible": [False, False, True]},
+                                ]),
+                    ]),
+                    direction="down",
+                    pad={"r": 10, "t": 10},
+                    showactive=True,
+                    x=0.01,
+                    xanchor="left",
+                    y=1.2,
+                    yanchor="top"
+                ),
+            ]
+
+        )
+        return(offl.plot(fig, show_link=False, output_type="div", include_plotlyjs=False))
+
+
+    def main_plot(self, graph_type, name=None, color=None, title=None,
+                x_title=None, y_title=None, x_range=None):
+        '''Plots the main graphs using plotly and to update traces that are used for plotting.
+        title : Title of the plot.
+        x_title : Title for x axis.
+        y_title : Title for y axis.
+        x_range : Passed to select particular range on x axis to show on plot.
+        graph_type : enter graph type that you want to plot ex:-go.Line, go.Bar, etc.
+        name : Names to be specified should be accordingly to data points that are parsed during the initialization of this class, list object.
+        color : List object of different colors.'''
+        fig = go.Figure()
+
+        for x_point, y_point, names, colors in zip(self.x_data, self.y_data, name, color):
+            traces = graph_type(x=x_point,
+                                y=y_point,
+                                name=names,
+                                marker_color=colors
+                                )
+            fig.add_trace(traces)
+
+        fig.update_layout(
+            title={
+                'text': title,
+                'y': 0.99,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': dict(
+                    family='Rockwell',
+                    color='dim gray',
+                    size=30),
+            },
+            # to add dropdown
+            updatemenus=[
+                dict(
+                    buttons=list([
+                        dict(
+                            args=["type", "line"],
+                            label="Line Graph",
+                            method="restyle"
+                        ),
+                        dict(
+                            args=["type", "bar"],
+                            label="Bar Graph",
+                            method="restyle"
+                        ),
+                    ]),
+                    direction="down",
+                    pad={"r": 10, "t": 10},
+                    showactive=True,
+                    x=0.01,
+                    xanchor="left",
+                    y=1.2,
+                    yanchor="top"
+                ),
+                dict(
+                    buttons=list([
+                        dict(
+                            label="Cummulative",
+                            method="update",
+                            args=[{"visible": [True, True, True]},
+                            ]),
+                        dict(
+                            label="Positive",
+                            method="update",
+                            args=[{"visible": [True, False, False]},
+                            ]),
+                        dict(
+                            label="Negative",
+                            method="update",
+                            args=[{"visible": [False, True, False]},
+                            ]),
+                        dict(
+                            label="Neutral",
+                            method="update",
+                            args=[{"visible": [False, False, True]},
+                            ]),
+                    ]),
+                    direction="down",
+                    pad={"r": 10, "t": 10},
+                    showactive=True,
+                    x=0.12,
+                    xanchor="left",
+                    y=1.2,
+                    yanchor="top"
+                ),
+            ],
             xaxis=dict(
                 title=x_title,
                 tickfont_size=14,
@@ -74,10 +237,12 @@ class Plot():
             barmode='group',
             bargap=0.2,
             bargroupgap=0.1,
-            plot_bgcolor='rgb(248,248,255)'
-
+            plot_bgcolor='rgb(248,248,255)',
         )
+
         return(offl.plot(fig, show_link=False, output_type="div", include_plotlyjs=False))
+
+
 
     def donnut_pie(self, label, value, center_name=None, title=None):
         '''Returns pie chart according to parameters passed.
@@ -97,6 +262,8 @@ class Plot():
             annotations=[dict(text=center_name, x=0.5, y=0.5, font_size=20, showarrow=False),
                             ])
         return(offl.plot(fig, show_link=False, output_type="div", include_plotlyjs=False))
+
+
 
     def create_table(self, locations, positive, negative, neutral, Title=None):
     
