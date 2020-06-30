c4fa1fcf3cda9d11855cb280b008936fe0c92df3
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-07-01 00:24:09 +0530

    Creating labels for ploting states, dates, sentiments

--- state_date_senti.py
+++ state_date_senti.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-def table_plot(df):
+def sunburst_plot(df):
     # Creating new columns for each sentiments; Positive, Negative, and Neutral
     df['Sentiment_1'] = df['Sentiments'].apply(lambda x: 1 if x == 1 else 0)
     df['Sentiment_neg_1'] = df['Sentiments'].apply(lambda x: 1 if x == -1 else 0)
